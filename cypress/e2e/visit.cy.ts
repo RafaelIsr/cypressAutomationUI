a73d9a07e3bd422e6dad2ce6c4d7b7ef9df06caf
RafaelIsr <r.a.israfilov@gmail.com>
2024-02-12 20:35:05 +0500

    first commit

---
+++ cypress/e2e/visit.cy.ts
@@ -0,0 +1,19 @@
+describe("WebSite", () => {
+  beforeEach(() => {
+    cy.visit("/home");
+  });
+  it("Verify page", () => {
+     cy.title().should("eq", "UI Test Automation Playground");
+  });
+});
+
+describe("WebSite", () => {
+  it("Verify page", () => {
+      cy.window().title().then((element) => {
+        cy.log(element)
+        expect(element).to.eq("UI Test Automation Playground");
+        expect(element === "UI Test Automation Playground").to.be.true;
+        cy.wrap(element).should("eq", "UI Test Automation Playground");
+      });
+  });
+});
